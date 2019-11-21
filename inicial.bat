0a631cf0ec4502506b1ea9a4911d9b2f07ed2eb8
adrianvibancorosales <adrianvibanco@outlook.com>
2019-11-21 14:51:21 -0600

    iniciando

---
+++ inicial.bat
@@ -0,0 +1,16 @@
+echo "cargando proyecto..."
+dotnet run
+
+pause
+
+echo "accediendo credenciales..."
+
+git init
+
+git add .
+
+
+
+git config --global user.email adrianvibanco@outlook.com
+git config --global user.name adrianvibancorosales
+
